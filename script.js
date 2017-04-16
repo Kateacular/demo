7d5996d95cf7061da3699a39b7c46bc64be9782f
Kateacular <kate.orr@detroitlabs.com>
2017-04-16 18:55:32 -0400

    adding functions for calling values

--- script.js
+++ script.js
@@ -18,17 +18,22 @@ var secondLetter;
 var chosenMonth;
 var chosenColor;
 
-localStorage.setItem('one', firstLetter);
-localStorage.setItem('two', secondLetter);
-localStorage.setItem('mos', chosenMonth);
-localStorage.setItem('col', chosenColor);
+var userLetOne;
+var userLetTwo;
+var userMonth;
+var userColor;
+
+var wordOne;
+var wordTwo;
 
 var myChoices =[];
 
 //JSON Messages
 
 //based on firstLetter
-var FirstAdj = {
+//JSON format:
+//var object = {key as string : value}
+var firstAdj = {
 					a: "Artless",
 					b: "Beslubbering",
 					c: "Churlish",
@@ -37,8 +42,20 @@ var FirstAdj = {
 					
 					};	
 
+
+
+
+
+
+
+
+
+
+
+
+
 //based on secondLetter
-var SecondAdj = {
+var secondAdj = {
 					a: "Beef-Witted",
 					b: "Boil-Brained",
 					c: "Clapper-Clawed",
@@ -46,6 +63,9 @@ var SecondAdj = {
 					e: "Earth-Vexing",
 					
 					};
+
+
+
 
 //based on chosenMonth
 var january =[ "Apple-John", "Baggage"];
@@ -61,15 +81,100 @@ var october = ["Puttock","Ratsbane"];
 var november = ["Scut","Skakinsmate"];
 var december = ["Strumpet","Varlot"];
 
-
+//based on chosenColor
+var red =["Bake cookies for someone.","Bring someone a souvenir within the week."];
+var orange = ["Write at least 5 positive sticky notes for strangers","Send a personal message to everyone you know that has a birthday today."];
+var yellow =["Leave quarters at a laundromat.","Donate at least 5 items of clothing."];
+var green=["Bring reusable bags to the grocery store","Switch to paperless mail.","Drink out of reusable cups."];
+var blue=["Write a handwritten letter and send it.","For every person you meet in the service industry today, say something kind to them."];
+var violet=["Donate used books to a library or senior center.","Compliment a stranger."];
+var black=["Write a positive comment on a website or blog","On your next break, go outside and don't bring your tech."];
+var white=["Give a gift card to someone just because.","Start a piggy bank for a cause."];
 
 $(document).ready(function() {
 
+$ (function getAdjFirst(){
+wordOne =localStorage.getItem('one');
+	for (var x in firstAdj) {
+	//console.log(firstAdj[wordOne]);
+    //console.log("testing adjective 1 call");
+	}
+});
+
+$ (function getAdjSecond(){
+wordTwo =localStorage.getItem('two');
+	for (var x in secondAdj) {
+	//console.log(secondAdj[wordTwo]);
+   // console.log("testing adjective 2 call");
+	}
+});
+
+
+$(function getNoun(){
+var chosenMonth =localStorage.getItem('mos');
+	if (chosenMonth == "Jan") {
+	userMonth = january[Math.floor(Math.random() * january.length)];
+	}
+	else if (chosenMonth == "Feb"){
+	userMonth = february[Math.floor(Math.random() * february.length)];
+	}
+	else if (chosenMonth == "Mar"){
+	userMonth = march[Math.floor(Math.random() * march.length)];
+	}
+	else if (chosenMonth == "Apr"){
+	userMonth = april[Math.floor(Math.random() * april.length)];
+	}
+	else if (chosenMonth == "May"){
+	userMonth = may[Math.floor(Math.random() * may.length)];
+	}
+	else if (chosenMonth == "Jun"){
+	userMonth = june[Math.floor(Math.random() * june.length)];
+	}
+	else if (chosenMonth == "Jul"){
+	userMonth = july[Math.floor(Math.random() * july.length)];
+	}
+	else if (chosenMonth == "Aug"){
+	userMonth = august[Math.floor(Math.random() * august.length)];
+	}
+	else if (chosenMonth == "Sep"){
+	userMonth = september[Math.floor(Math.random() * september.length)];
+	}
+	else if (chosenMonth == "Oct"){
+	userMonth = october[Math.floor(Math.random() * october.length)];
+	}
+	else if (chosenMonth == "Nov"){
+	userMonth = november[Math.floor(Math.random() * november.length)];
+	}
+	else if (chosenMonth == "Dec"){
+	userMonth = december[Math.floor(Math.random() * december.length)];
+	}
+	else {
+	alert("No Month Selected");
+	}
+//var wordOne = "Checking Variable Recall";
+alert(userMonth);
+});
+
+$ (function storeInsult() {
+	console.log("storingInsult");
+var userInsult = (firstAdj[wordOne])+(secondAdj[wordTwo])+userMonth;
+localStorage.setItem('fullInsult',userInsult);
+});
+
+
+$ (function getInsult() {
+	console.log("calling insult function");
+var sickBurn =localStorage.getItem('fullInsult');
+console.log(sickBurn);
+});
 
 $ ('#submit').click(function() {
 	firstLetter = $('input[name=francis]').val();
+	localStorage.setItem('one', firstLetter);
 	secondLetter = $('input[name=sally]').val();
+	localStorage.setItem('two', secondLetter);
 	chosenMonth = $('option:selected').val();
+	localStorage.setItem('mos', chosenMonth);
 	if ((firstLetter === " ") || (firstLetter ==="") || (secondLetter === " ") || (secondLetter === "")) {
 		console.log("space or blank entered");
 	alert ("Please enter a letter.");
@@ -79,32 +184,21 @@ $ ('#submit').click(function() {
 	// alert ("Letters only, please.");
 	// }
 	else {
-		//myChoices.push(firstLetter);
 		alert (firstLetter + secondLetter + chosenMonth);
 		console.log(firstLetter + secondLetter + chosenMonth);
+		getAdjFirst();
+		getAdjSecond();
+		storeInsult();
 	}
 });
 	
 $ ('#submitTwo').click(function() {
 	//chosenColor = $('input[name=colorChoice]').val();
 	chosenColor = $('option:selected').val();
+	localStorage.setItem('col', chosenColor);
 		console.log(chosenColor);
 		alert (chosenColor);
-		
 	});
-
-
-$ (function getInsult() {
-	console.log("calling insult function");
-//var wordOne =localStorage.getItem('one');
-var wordOne = "Checking Variable Recall";
-alert(wordOne);
-});
-
-
-
-
-
 
 
 
